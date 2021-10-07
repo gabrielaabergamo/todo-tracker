d2982ef871b540ffe50dba53f64386022743b485
gabrielaabergamo <gabiibergamo@gmail.com>
2021-10-07 18:50:27 -0300

    local storage

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+window.onload = () =>{
 let formUser = document.querySelector('form')
 let todoList = document.querySelector('ul')
 let botaoClear = document.querySelector('button')
 let inputUser = document.getElementById('user-todo')
 
+let todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+
+localStorage.setItem('todos', JSON.stringify(todosArray))
+
+let storage = JSON.parse(localStorage.getItem('todos'))
+
 formUser.addEventListener('submit', function(e){
     e.preventDefault()
+
+    todosArray.push(inputUser.value)
+    localStorage.setItem('todos', JSON.stringify(todosArray))
+
     todoMaker(inputUser.value)
     inputUser.value = ''
     
@@ -16,10 +27,15 @@ function todoMaker(valor){
     todoList.appendChild(addTodo)
 }
 
+for (var i = 0; i < storage.length; i++) {
+    todoMaker(storage[i]);
+}
 
-botaoClear.addEventListener('click', function(){
+botaoClear.addEventListener('click', function(e){
+    e.preventDefault()
+    localStorage.clear()
     while(todoList.firstChild){
         todoList.removeChild(todoList.firstChild)
     }
 })
-
+}
